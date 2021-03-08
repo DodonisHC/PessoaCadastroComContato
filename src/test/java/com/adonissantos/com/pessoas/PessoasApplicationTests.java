1e1c48bc1a748ed147397c7bb0c9e55e07c1562c
adonis.ingadigital <adonis@ingadigital.com.br>
2021-03-08 02:40:10 -0300

    teste prifile commit

--- src/test/java/com/adonissantos/com/pessoas/PessoasApplicationTests.java
+++ src/test/java/com/adonissantos/com/pessoas/PessoasApplicationTests.java
@@ -2,8 +2,10 @@ package com.adonissantos.com.pessoas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PessoasApplicationTests {
 
 	@Test
